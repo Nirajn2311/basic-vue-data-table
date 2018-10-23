1c4d9b79b33bebd9a889ee14eefc1052edf27b4d
May Kittens Devour Your Soul <yoshimitsu002@gmail.com>
2018-10-23 23:11:10 +0200

    Update DataTable.vue

--- src/components/DataTable.vue
+++ src/components/DataTable.vue
@@ -1,33 +1,35 @@
 <template>
-  <div class="col-md-12">
+<div>
+  <div class="col-md-12"  v-if="users.length">
     <div class="form-group">
-      <input type="text" class="form-control" v-model="search" placeholder="Search by user name">
+      <input type="text" class="form-control border border-info" v-model="search" placeholder="pretraži po svemu">
     </div>
     <div class="table-responsive">
       <table class="table table-striped table-bordered" style="width:100%">
           <thead width="400px">
               <tr>
                   <th scope="col">#</th>
-                  <th scope="col" @click="sort('name')">Name <i class="fas fa-sort-alpha-down float-right"></i></th>
-                  <th scope="col" @click="sort('email')">Email<i class="fas fa-sort-alpha-down float-right"></i></th>
-                  <th scope="col">City</th>
-                  <th scope="col">Phone </th>
+                  <th scope="col" @click="sort('signatura')">Signatura<i class="fas fa-sort-alpha-down float-right"></i></th>
+                  <th scope="col" @click="sort('autor')">Autor <i class="fas fa-sort-alpha-down float-right"></i></th>
+                  <th scope="col" @click="sort('naslov')">Naslov <i class="fas fa-sort-alpha-down float-right"></i></th>
               </tr>
           </thead>
           <tbody>
               <tr v-for="(user, index) in (sortedActivity, filteredList)" :key="index">
                 <td>{{index + 1}}</td>
-                <td>{{user.name}}</td>
-                <td>{{user.email}}</td>
-                <td>{{user.address.city}}</td>
-                <td>{{user.phone}}</td>
+                <td>{{signatura}}</td>
+                <td>{{autor}}</td>
+                <td>{{naslov}}</td>
               </tr>
           </tbody>
       </table>
     </div>
-    <p><button @click="prevPage" class="float-left"><i class="fas fa-arrow-left"></i> Previous</button> <button @click="nextPage" class="float-right">Next <i class="fas fa-arrow-right"></i></button></p>
+ <button @click="prevPage" class="float-left btn btn-outline-info btn-sm"><i class="fas fa-arrow-left"></i> Previous</button> 
+ <button @click="nextPage" class="float-right btn btn-outline-info btn-sm">Next <i class="fas fa-arrow-right"></i></button>
   </div>
+    </div>
 </template>
+
 
 <script>
 /*eslint-disable*/
@@ -61,7 +63,8 @@ export default {
 
   computed: {
     sortedActivity:function() {
-      return this.users.sort((a,b) => {
+
+    return this.users.sort((a,b) => {
         let modifier = 1;
         if(this.currentSortDir === 'desc') modifier = -1;
         if(a[this.currentSort] < b[this.currentSort]) return -1 * modifier;
@@ -76,11 +79,10 @@ export default {
 
     filteredList () {
       return this.users.filter((data) => {
-        let email = data.email.toLowerCase().match(this.search.toLowerCase());
-        let name = data.name.toLowerCase().match(this.search.toLowerCase());
-        let city = data.address.city.toLowerCase().match(this.search.toLowerCase());
-        let phone = data.phone.toLowerCase().match(this.search.toLowerCase());
-        return email || name || city || phone;
+        let signatura = data.signatura.toLowerCase().match(this.search.toLowerCase());
+        let autor = data.autor.toLowerCase().match(this.search.toLowerCase());
+        let naslov = data.naslov.city.toLowerCase().match(this.search.toLowerCase());
+        return signatura || autor|| naslov;
       }).filter((row, index) => {
         let start = (this.currentPage-1)*this.pageSize;
         let end = this.currentPage*this.pageSize;
@@ -90,7 +92,7 @@ export default {
   },
 
   created () {
-    axios.get('https://jsonplaceholder.typicode.com/users')
+    return axios.get('https://gist.githubusercontent.com/diomed/da5b879b026486e9bcab13dc2946d1a0/raw/cb2525b8cbd4bb6f5ac71da42f71d2e78f8703e8/library.json')
       .then(response => {
         this.users = response.data
       })

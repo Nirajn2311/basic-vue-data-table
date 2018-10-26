21413b43b5fe6063899d8d54898de5e702b918f1
Niraj Nandish <nirajnandish@icloud.com>
2018-10-26 20:17:27 +0400

    solve-cats-problems

--- src/components/DataTable.vue
+++ src/components/DataTable.vue
@@ -17,9 +17,9 @@
           <tbody>
               <tr v-for="(user, index) in (sortedActivity, filteredList)" :key="index">
                 <td>{{index + 1}}</td>
-                <td>{{signatura}}</td>
-                <td>{{autor}}</td>
-                <td>{{naslov}}</td>
+                <td>{{user.signatura}}</td>
+                <td>{{user.autor}}</td>
+                <td>{{user.naslov}}</td>
               </tr>
           </tbody>
       </table>
@@ -36,80 +36,88 @@
 import axios from 'axios';
 
 export default {
-  data: () => ({
-    users: [],
-    currentSort:'name',
-    currentSortDir:'asc',
-    search: '',
-    searchSelection: '',
-    pageSize: 5,
-    currentPage: 1
-  }),
+	data: () => ({
+		users: [],
+		currentSort: 'name',
+		currentSortDir: 'asc',
+		search: '',
+		searchSelection: '',
+		pageSize: 5,
+		currentPage: 1
+	}),
 
-  methods:{
-    sort:function(s) {
-      if(s === this.currentSort) {
-        this.currentSortDir = this.currentSortDir==='asc'?'desc':'asc';
-      }
-      this.currentSort = s;
-    },
-    nextPage:function() {
-      if((this.currentPage*this.pageSize) < this.users.length) this.currentPage++;
-    },
-    prevPage:function() {
-      if(this.currentPage > 1) this.currentPage--;
-    }
-  },
+	methods: {
+		sort: function(s) {
+			if (s === this.currentSort) {
+				this.currentSortDir = this.currentSortDir === 'asc' ? 'desc' : 'asc';
+			}
+			this.currentSort = s;
+		},
+		nextPage: function() {
+			if (this.currentPage * this.pageSize < this.users.length)
+				this.currentPage++;
+		},
+		prevPage: function() {
+			if (this.currentPage > 1) this.currentPage--;
+		}
+	},
 
-  computed: {
-    sortedActivity:function() {
+	computed: {
+		sortedActivity: function() {
+			return this.users
+				.sort((a, b) => {
+					let modifier = 1;
+					if (this.currentSortDir === 'desc') modifier = -1;
+					if (a[this.currentSort] < b[this.currentSort]) return -1 * modifier;
+					if (a[this.currentSort] > b[this.currentSort]) return 1 * modifier;
+					return 0;
+				})
+				.filter((row, index) => {
+					let start = (this.currentPage - 1) * this.pageSize;
+					let end = this.currentPage * this.pageSize;
+					if (index >= start && index < end) return true;
+				});
+		},
 
-    return this.users.sort((a,b) => {
-        let modifier = 1;
-        if(this.currentSortDir === 'desc') modifier = -1;
-        if(a[this.currentSort] < b[this.currentSort]) return -1 * modifier;
-        if(a[this.currentSort] > b[this.currentSort]) return 1 * modifier;
-        return 0;
-      }).filter((row, index) => {
-        let start = (this.currentPage-1)*this.pageSize;
-        let end = this.currentPage*this.pageSize;
-        if(index >= start && index < end) return true;
-      });
-    },
+		filteredList: function() {
+			return this.users
+				.filter(data => {
+					let signatura = data.signatura.match(this.search);
+					let autor = data.autor.toLowerCase().match(this.search.toLowerCase());
+					let naslov = data.naslov
+						.toLowerCase()
+						.match(this.search.toLowerCase());
+					return signatura || autor || naslov;
+				})
+				.filter((row, index) => {
+					let start = (this.currentPage - 1) * this.pageSize;
+					let end = this.currentPage * this.pageSize;
+					if (index >= start && index < end) return true;
+				});
+		}
+	},
 
-    filteredList () {
-      return this.users.filter((data) => {
-        let signatura = data.signatura.toLowerCase().match(this.search.toLowerCase());
-        let autor = data.autor.toLowerCase().match(this.search.toLowerCase());
-        let naslov = data.naslov.city.toLowerCase().match(this.search.toLowerCase());
-        return signatura || autor|| naslov;
-      }).filter((row, index) => {
-        let start = (this.currentPage-1)*this.pageSize;
-        let end = this.currentPage*this.pageSize;
-        if(index >= start && index < end) return true;
-      });
-    }
-  },
-
-  created () {
-    return axios.get('https://gist.githubusercontent.com/diomed/da5b879b026486e9bcab13dc2946d1a0/raw/cb2525b8cbd4bb6f5ac71da42f71d2e78f8703e8/library.json')
-      .then(response => {
-        this.users = response.data
-      })
-  },
-
-}
+	created() {
+		return axios
+			.get(
+				'https://gist.githubusercontent.com/Nirajn2311/b89419c3d71673dfc6890783b26a1fff/raw/860c530d7deb60b6de0944d9722cf0285db92ccd/names.json'
+			)
+			.then(response => {
+				this.users = response.data.items;
+				console.log(this.users);
+			});
+	}
+};
 </script>
 
 <style>
 th {
-  cursor:pointer;
-  /* width: 500px !important; */
-  white-space: nowrap;
+	cursor: pointer;
+	/* width: 500px !important; */
+	white-space: nowrap;
 }
 
 tr {
-  white-space: nowrap;
+	white-space: nowrap;
 }
-
 </style>
